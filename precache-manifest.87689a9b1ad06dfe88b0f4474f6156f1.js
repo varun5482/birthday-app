f231154f7eb25419bffeb63f60445db79a87fd72
varun5482 <varunmukherjee19@gmail.com>
2020-09-19 23:09:06 +0530

    Updates

---
+++ precache-manifest.87689a9b1ad06dfe88b0f4474f6156f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2073fec9aba9a75dd45fa0fd1d11f708",
+    "url": "/birthday-app/git/index.html"
+  },
+  {
+    "revision": "1c77e55b944ff6fdb9d8",
+    "url": "/birthday-app/git/static/css/main.5e87f21e.chunk.css"
+  },
+  {
+    "revision": "3775a00e433e49a209e2",
+    "url": "/birthday-app/git/static/js/2.c8bb3bac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-app/git/static/js/2.c8bb3bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c77e55b944ff6fdb9d8",
+    "url": "/birthday-app/git/static/js/main.d120b8f9.chunk.js"
+  },
+  {
+    "revision": "b327391a411f07ed4c92",
+    "url": "/birthday-app/git/static/js/runtime-main.1b5e949a.js"
+  }
+]);
