1e4b83d3fbdf1a880f87c037e0a79a766823119a
varun5482 <varunmukherjee19@gmail.com>
2020-09-19 23:24:28 +0530

    Updates

---
+++ precache-manifest.631a19af78a2de665f7060d19a018c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0345bcc0178a40ef9ed1f3e3804726fa",
+    "url": "/birthday-app/index.html"
+  },
+  {
+    "revision": "1c77e55b944ff6fdb9d8",
+    "url": "/birthday-app/static/css/main.5e87f21e.chunk.css"
+  },
+  {
+    "revision": "3775a00e433e49a209e2",
+    "url": "/birthday-app/static/js/2.c8bb3bac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-app/static/js/2.c8bb3bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c77e55b944ff6fdb9d8",
+    "url": "/birthday-app/static/js/main.d120b8f9.chunk.js"
+  },
+  {
+    "revision": "1465b00e6f874f060fb0",
+    "url": "/birthday-app/static/js/runtime-main.1dda3dd2.js"
+  }
+]);
